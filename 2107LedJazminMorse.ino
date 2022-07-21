7b19f7a2b2f65c4fc88f12e97d54a17dea934029
JazCR0106 <109745368+JazCR0106@users.noreply.github.com>
2022-07-21 11:31:25 -0300

    Add files via upload
    Este skech esta conectado en el pin 13 a un led e informa un nombre en código morse.
    J = . _ _ _
    A = . _
    Z = _ _ . .
    M = _ _
    I = . .
    N = _ .

---
+++ 2107LedJazminMorse.ino
@@ -0,0 +1,79 @@
+int blue = 13 ;
+
+void setup()
+{
+ pinMode(blue, OUTPUT); 
+}
+void loop()
+{
+   //Letra J .---
+   digitalWrite(blue,HIGH);
+   delay(1000);
+   digitalWrite(blue,LOW);
+   delay(1000);
+   digitalWrite(blue,HIGH);
+   delay(3000);
+   digitalWrite(blue,LOW);
+   delay(1000);
+   digitalWrite(blue,HIGH);
+   delay(3000);
+   digitalWrite(blue,LOW);
+   delay(1000);
+   digitalWrite(blue,HIGH);
+   delay(3000);
+   digitalWrite(blue,LOW);
+   delay(1000);
+   //Letra A .-
+   digitalWrite(blue,HIGH);
+   delay(1000);
+   digitalWrite(blue,LOW);
+   delay(1000);
+   digitalWrite(blue,HIGH);
+   delay(3000);
+   digitalWrite(blue,LOW);
+   delay(1000);
+   //Letra Z --..
+   digitalWrite(blue,HIGH);
+   delay(3000);
+   digitalWrite(blue,LOW);
+   delay(1000);
+   digitalWrite(blue,HIGH);
+   delay(3000);
+   digitalWrite(blue,LOW);
+   delay(1000);
+   digitalWrite(blue,HIGH);
+   delay(1000);
+   digitalWrite(blue,LOW);
+   delay(1000);
+   digitalWrite(blue,HIGH);
+   delay(1000);
+   digitalWrite(blue,LOW);
+   delay(1000);
+   //Letra M --
+   digitalWrite(blue,HIGH);
+   delay(3000);
+   digitalWrite(blue,LOW);
+   delay(1000);
+   digitalWrite(blue,HIGH);
+   delay(3000);
+   digitalWrite(blue,LOW);
+   delay(1000);
+   //Letra I ..
+   digitalWrite(blue,HIGH);
+   delay(1000);
+   digitalWrite(blue,LOW);
+   delay(1000);
+   digitalWrite(blue,HIGH);
+   delay(1000);
+   digitalWrite(blue,LOW);
+   delay(1000);
+   //Letra N -.
+   digitalWrite(blue,HIGH);
+   delay(3000);
+   digitalWrite(blue,LOW);
+   delay(1000);
+   digitalWrite(blue,HIGH);
+   delay(1000);
+   digitalWrite(blue,LOW);
+   delay(1000);
+}
